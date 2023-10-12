33fb123762ec63d9a57817a11cf2ff1f4a393c31
yuha-yuha <ishizushimadev@gmail.com>
2023-10-12 15:48:37 +0900

    QuizeFetchハンドラの実装

--- controller/Handler/GetQuize.go
+++ controller/Handler/GetQuize.go
@@ -1,7 +1,24 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"math/rand"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yuha-yuha/2023KosenFes_KosenGo_Backend.git/model"
+)
 
 func QuizeFetch(ctx *gin.Context) {
+	rand.Seed(time.Now().UnixNano())
+	index := rand.Intn(len(model.GetQuizeList()))
+
+	quize, err := model.QuizeSelect(index)
+
+	if err != nil {
+		ctx.Abort()
+		panic(err)
+	}
+
+	ctx.JSON(200, quize)
 
 }
